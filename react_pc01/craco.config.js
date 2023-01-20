924b4ecd8bb90971de734c608696940e833b3cf2
AndersonPetall <andersonpetall@gmail.com>
2023-01-20 02:07:08 -0500

    Update craco.config.js

--- react_pc01/craco.config.js
+++ react_pc01/craco.config.js
@@ -2,14 +2,12 @@
 const path = require('path')
 const { whenProd, getPlugin, pluginByName } = require('@craco/craco')
 module.exports = {
-module.exports = {
   // webpack 配置
   webpack: {
     // 配置别名路径
     alias: {
       // 约定：使用 @ 表示 src 文件所在路径
       '@': path.resolve(__dirname, 'src')
-    },
-    //cdn配置
+    }
   }
 }
